1acd09b6429645608ec4680ec92063c2ea113195
David Ragone <dmragone@gmail.com>
2014-11-15 21:01:47 -0800

    Continue moving logic into cardsService

--- front/js/controllers/cardsCtrl.js
+++ front/js/controllers/cardsCtrl.js
@@ -1,148 +1,176 @@
-setReviewGrader.controller('cardsCtrl',
-  function ($scope, $localStorage, cardsService) {
+/**
+ * cardCtrl - TODO: rename
+ *
+ * Main controller for set review grading
+ */
+setReviewGrader.controller('cardsCtrl', function ($scope, $localStorage, cardsService) {
+  /**
+   * sets: Manually maintained list of magic the gathering sets.
+   *   Ordered chronologically descending.
+   */
+  $scope.sets = [
+    'KTK',
+    'M15',
+    'JOU', 'BNG', 'THS',
+    'M14',
+  ];
 
-    $scope.setCard = function(color, card) {
-      // Before we move to the next card:
-      //   If previous card has not been graded, make grade the default 'C'
-      if (typeof($scope.currentCard) != 'undefined' && typeof($scope.currentCard.graded) == 'undefined') {
-        $scope.gradeCard('C');
-      }
-      $scope.currentColor = color;
-      $scope.currentCard = card;
-      // Now that we're on the new card:
-      //   If you hadn't graded it previously, set it to 'C' (manually, w/o incrementing, etc.)
-      if (typeof($scope.currentCard.grade) == 'undefined') {
-        $scope.currentCard.grade = 'C';
-      }
-      if (typeof(resetCardLocation) != 'undefined') { resetCardLocation(); }
-    };
+  /**
+   * currentSet: The set the user is grading. Defaults to the most recent
+   */
+  $scope.currentSet = $scope.sets[0];
 
-    // https://github.com/gsklee/ngStorage
-    $scope.$storage = $localStorage;
+  /**
+   * Initialize the card & set data (via cardsService)
+   */
+  cardsService.fetchData($scope.currentSet).then(function(data) {
+    $scope.cards = data.cards;
+    $scope.set   = data.set;
+  });
 
-    // TODO -move all this into the card service
-    if ($scope.$storage.setReviewGrader) {
-      console.log('LOADING FROM LOCAL STORAGE');
-      // data stored in local storage as single key with large object... bad idea?
-      var data = $scope.$storage.setReviewGrader;
-      $scope.set = data.set;
-      $scope.cardsByColor = data.cardsByColor;
-      $scope.colors = data.colors;
+  /////////////////////////////////////////////////////////////////////////////////
+
+  $scope.setCard = function(color, card) {
+    // Before we move to the next card:
+    //   If previous card has not been graded, make grade the default 'C'
+    if (typeof($scope.currentCard) != 'undefined' && typeof($scope.currentCard.graded) == 'undefined') {
+      $scope.gradeCard('C');
+    }
+    $scope.currentColor = color;
+    $scope.currentCard = card;
+    // Now that we're on the new card:
+    //   If you hadn't graded it previously, set it to 'C' (manually, w/o incrementing, etc.)
+    if (typeof($scope.currentCard.grade) == 'undefined') {
+      $scope.currentCard.grade = 'C';
+    }
+    if (typeof(resetCardLocation) != 'undefined') { resetCardLocation(); }
+  };
+
+  // https://github.com/gsklee/ngStorage
+  $scope.$storage = $localStorage;
+
+  // TODO -move all this into the card service
+  if ($scope.$storage.setReviewGrader) {
+    console.log('LOADING FROM LOCAL STORAGE');
+    // data stored in local storage as single key with large object... bad idea?
+    var data = $scope.$storage.setReviewGrader;
+    $scope.set = data.set;
+    $scope.cardsByColor = data.cardsByColor;
+    $scope.colors = data.colors;
+    $scope.setCard('White', $scope.cardsByColor.White[0]);
+    //TODO - figure out how to get the card to be displayed in the correct location
+    //resetCardLocation();
+  }
+  else {
+    $scope.cardsByColor = cardsService.initializeCardsByColor();
+    $scope.colors = Object.keys($scope.cardsByColor);
+
+    cardsService.fetchData().then(function(data) {
+      $scope.set = cardsService.set(data);
+      cardsService.groupCardsByColor($scope.cardsByColor, data.cards);
       $scope.setCard('White', $scope.cardsByColor.White[0]);
-      //TODO - figure out how to get the card to be displayed in the correct location
-      //resetCardLocation();
+    });
+  }
+
+  $scope.percentGraded = function() {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+    // Shift
+    if ((typeof($scope.set) != 'undefined') && typeof($scope.set.gradedCount) != 'undefined') {
+      var value = ($scope.set.gradedCount / $scope.set.cardCount) * 100
+      value = value.toString().split('e');
+      value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + 1) : 1)));
+      // Shift back
+      value = value.toString().split('e');
+      return +(value[0] + 'e' + (value[1] ? (+value[1] - 1) : -1)) + "%";
     }
     else {
-      $scope.cardsByColor = cardsService.initializeCardsByColor();
-      $scope.colors = Object.keys($scope.cardsByColor);
+      return '0%';
+    }
+  };
 
-      cardsService.fetchData().then(function(data) {
-        $scope.set = cardsService.set(data);
-        cardsService.groupCardsByColor($scope.cardsByColor, data.cards);
+  $scope.cardFilters = {
+    colors: {}
+  };
+
+  $scope.gradeCard = function(grade) {
+    if ($scope.currentCard) {
+      // Grade card
+      $scope.currentCard.grade = grade;
+      // increment count if not previously graded
+      if (typeof($scope.currentCard.graded) == 'undefined') {
+        $scope.set.gradedCount++;
+        $scope.currentCard.graded = true;
+      }
+
+      // Move card
+      resetCardLocation();
+
+      // Save everything to local storage
+      var localStorageObject = {
+        set: $scope.set,
+        cardsByColor: $scope.cardsByColor,
+        colors: $scope.colors
+      };
+      $scope.$storage.setReviewGrader = localStorageObject;
+    }
+  };
+
+  $scope.nextCard = function() {
+    // find index of current card in current color
+    var index = $scope.cardsByColor[$scope.currentColor].indexOf($scope.currentCard);
+    // if there's a card in the next spot, make that the current card
+    var nextCard = $scope.cardsByColor[$scope.currentColor][index + 1];
+    if (typeof(nextCard) != 'undefined') {
+      $scope.setCard($scope.currentColor, nextCard);
+    }
+    // else go to next color, first card there
+    else {
+      var nextColor = $scope.colors[$scope.colors.indexOf($scope.currentColor) + 1];
+      // if we're on the last color, then the next one is undefined, so we'll start over at White
+      if (typeof(nextColor) == 'undefined') {
         $scope.setCard('White', $scope.cardsByColor.White[0]);
-      });
+      }
+      else {
+        $scope.setCard(nextColor, $scope.cardsByColor[nextColor][0]);
+      }
     }
 
-    $scope.percentGraded = function() {
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
-      // Shift
-      if ((typeof($scope.set) != 'undefined') && typeof($scope.set.gradedCount) != 'undefined') {
-        var value = ($scope.set.gradedCount / $scope.set.cardCount) * 100
-        value = value.toString().split('e');
-        value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + 1) : 1)));
-        // Shift back
-        value = value.toString().split('e');
-        return +(value[0] + 'e' + (value[1] ? (+value[1] - 1) : -1)) + "%";
+    resetCardLocation();
+  };
+
+  $scope.prevCard = function() {
+    // find index of current card in current color
+    var index = $scope.cardsByColor[$scope.currentColor].indexOf($scope.currentCard);
+    // if there's a card in the previous spot, make that the current card
+    var prevCard = $scope.cardsByColor[$scope.currentColor][index - 1];
+    if (typeof(prevCard) != 'undefined') {
+      $scope.setCard($scope.currentColor, prevCard);
+    }
+    // else go to prev color, last card there
+    else {
+      var prevColor = $scope.colors[$scope.colors.indexOf($scope.currentColor) - 1];
+      // if we're on the first color, then the previous one is undefined, so we'll go to end (land)
+      if (typeof(prevColor) == 'undefined') {
+        $scope.setCard('Land', $scope.cardsByColor.Land[$scope.cardsByColor.Land.length - 1]);
       }
       else {
-        return '0%';
+        $scope.setCard(prevColor, $scope.cardsByColor[prevColor][$scope.cardsByColor[prevColor].length -1]);
       }
-    };
+    }
+    resetCardLocation();
+  };
 
-    $scope.cardFilters = {
-      colors: {}
-    };
-
-    $scope.gradeCard = function(grade) {
-      if ($scope.currentCard) {
-        // Grade card
-        $scope.currentCard.grade = grade;
-        // increment count if not previously graded
-        if (typeof($scope.currentCard.graded) == 'undefined') {
-          $scope.set.gradedCount++;
-          $scope.currentCard.graded = true;
-        }
-
-        // Move card
-        resetCardLocation();
-
-        // Save everything to local storage
-        var localStorageObject = {
-          set: $scope.set,
-          cardsByColor: $scope.cardsByColor,
-          colors: $scope.colors
-        };
-        $scope.$storage.setReviewGrader = localStorageObject;
-      }
-    };
-
-    $scope.nextCard = function() {
-      // find index of current card in current color
-      var index = $scope.cardsByColor[$scope.currentColor].indexOf($scope.currentCard);
-      // if there's a card in the next spot, make that the current card
-      var nextCard = $scope.cardsByColor[$scope.currentColor][index + 1];
-      if (typeof(nextCard) != 'undefined') {
-        $scope.setCard($scope.currentColor, nextCard);
-      }
-      // else go to next color, first card there
-      else {
-        var nextColor = $scope.colors[$scope.colors.indexOf($scope.currentColor) + 1];
-        // if we're on the last color, then the next one is undefined, so we'll start over at White
-        if (typeof(nextColor) == 'undefined') {
-          $scope.setCard('White', $scope.cardsByColor.White[0]);
-        }
-        else {
-          $scope.setCard(nextColor, $scope.cardsByColor[nextColor][0]);
-        }
-      }
-
-      resetCardLocation();
-    };
-
-    $scope.prevCard = function() {
-      // find index of current card in current color
-      var index = $scope.cardsByColor[$scope.currentColor].indexOf($scope.currentCard);
-      // if there's a card in the previous spot, make that the current card
-      var prevCard = $scope.cardsByColor[$scope.currentColor][index - 1];
-      if (typeof(prevCard) != 'undefined') {
-        $scope.setCard($scope.currentColor, prevCard);
-      }
-      // else go to prev color, last card there
-      else {
-        var prevColor = $scope.colors[$scope.colors.indexOf($scope.currentColor) - 1];
-        // if we're on the first color, then the previous one is undefined, so we'll go to end (land)
-        if (typeof(prevColor) == 'undefined') {
-          $scope.setCard('Land', $scope.cardsByColor.Land[$scope.cardsByColor.Land.length - 1]);
-        }
-        else {
-          $scope.setCard(prevColor, $scope.cardsByColor[prevColor][$scope.cardsByColor[prevColor].length -1]);
-        }
-      }
-      resetCardLocation();
-    };
-
-    var resetCardLocation = function() {
-      var image = document.getElementById('currentCard');
-      // Set at default location if not yet graded
-      if (typeof($scope.currentCard.graded) == 'undefined') {
-        var defaultGradeLocation = document.getElementById('gradeC');
-        defaultGradeLocation.appendChild(image);
-      }
-      // Otherwise set where the grade is
-      else {
-        var currentGradeLocation = document.getElementById('grade' + $scope.currentCard.grade);
-        currentGradeLocation.appendChild(image);
-      }
-    };
-
+  var resetCardLocation = function() {
+    var image = document.getElementById('currentCard');
+    // Set at default location if not yet graded
+    if (typeof($scope.currentCard.graded) == 'undefined') {
+      var defaultGradeLocation = document.getElementById('gradeC');
+      defaultGradeLocation.appendChild(image);
+    }
+    // Otherwise set where the grade is
+    else {
+      var currentGradeLocation = document.getElementById('grade' + $scope.currentCard.grade);
+      currentGradeLocation.appendChild(image);
+    }
+  };
 });
--- front/js/services/cards.js
+++ front/js/services/cards.js
@@ -1,51 +1,192 @@
-setReviewGrader.service('cardsService', function ($http) {
-  this.fetchData = function(callback) {
-    // Return a promise that has response data
-    return $http.get("http://mtgjson.com/json/M15.json")
-  // Or, .success(function(data) {}...
-      .then(function(response) { return response.data; });
-// TODO: return array of sorted cards
-  };
+/**
+ * cardsService
+ *
+ * Manages card & set info, including fetching data from server & storing locally
+ *   A card object looks like: 
+ *   {
+ *     "layout":"normal",
+ *     "type":"Creature â€” Spirit",
+ *     "types":["Creature"],
+ *     "colors":["Black"],
+ *     "multiverseid":383175,
+ *     "name":"Accursed Spirit",
+ *     "subtypes":["Spirit"],
+ *     "cmc":4,
+ *     "rarity":"Common",
+ *     "artist":"Kev Walker",
+ *     "power":"3",
+ *     "toughness":"2",
+ *     "manaCost":"{3}{B}",
+ *     "text":"Intimidate (This creature can't be blocked except by artifact creatures and/or creatures that share a color with it.)",
+ *     "flavor":"Many have heard the slither of dragging armor and the soft squelch of its voice. But only its victims ever meet its icy gaze.",
+ *     "number":"85",
+ *     "imageName":"accursed spirit"
+ *   }
+ *
+ */
+setReviewGrader.factory('cardsService', function ($http, $localStorage, $q) {
+  var data, sortedCards, set;
 
-  this.set = function(data) {
-    return {
-      cardCount: data.cards.length,
-      gradedCount: 0,
-      name: data.name
-    };
-  };
+  return {
+    /**
+     * Gets card & set data from localStorage or mtgjson.com. Returns the data
+     *   in the form of a promise, which, when resolved, will contain keys for
+     *   the cards & set.
+     *
+     * @method fetchData
+     * @param {String} : setCode - the three-letter representation of the magic set
+     * @return {Promise}
+     */
+    fetchData: function(setCode) {
+      var deferred = $q.defer();
+      // not yet in memory
+      if (typeof(data) === "undefined") {
+        // not stored in local storage, fetch from url
+        if (!dataStoredLocally(setCode)) {
+          // TODO - move url base to constant
+          $http.get("http://mtgjson.com/json/" + setCode + ".json")
+            .success(function(response) {
+              sortedCards = sortCards(response.data.cards);
+              set = {
+                name: response.name,
+                code: response.code,
+                cardCount: sortedCards.length;
+                gradedCount: 0
+              };
+              storeLocally(set, sortedCards);
+              deferred.resolve({
+                sortedCards: sortedCards,
+                set: set
+              })
+            }).error(function(msg, code) {
+              deferred.reject(msg);
+              console.log(msg, code);
+            });
+        // load from local storage
+        } else {
+          data = loadFromLocalStorage(setCode);
+          sortedCards = data.sortedCards;
+          set = data.set;
+          deferred.resolve({
+            sortedCards: sortedCards,
+            set: set
+          });
+        }
+      // in memory, load from there
+      } else {
+        deferred.resolve({
+          sortedCards: sortedCards,
+          set: set
+        });
+      }
+      return deferred.promise;
+    },
 
-  this.initializeCardsByColor = function() {
-    return {
-      White: [],
-      Blue: [],
-      Black: [],
-      Red: [],
-      Green: [],
-      Multicolored: [],
-      Artifact: [],
-      Land: [],
-    };
-  };
+    /**
+     * Whether or not the given set's data is already in localStorage
+     *
+     * @method dataStoredLocally
+     * @param {String} set - the three-letter representation of the magic set
+     * @return {Boolean}
+     */
+    dataStoredLocally: function(set) {
+      return typeof($localStorage.setReviewGraderData) !== "undefined" &&
+        typeof($localStorage.setReviewGrader[set]) !== "undefined";
+    },
 
-  this.groupCardsByColor = function(cardsByColor, allCards) {
-    var card;
+    /**
+     * Loads the current set & set's cards from localStorage
+     *
+     * @method loadFromLocalStorage
+     * @param {String} setCode - the three-letter representation of the magic set
+     * @return {Object} - { set: {//set data}, cards: {//card data} }
+     */
+    loadFromLocalStorage: function(setCode) {
+      return {
+        set: $localStorage.setReviewGrader[setCode].set,
+        sortedCards: sortCards($localStorage.setReviewGrader[setCode].sortedCards)
+      };
+    },
 
-    for (var i = 0; i < allCards.length; i++) {
-      card = allCards[i];
+    /**
+     * Stores data in local storage
+     *
+     * @method storeLocally
+     * @param {Object} : set
+     * @param {Object} : sortedCards
+     */
+    storeLocally: function(set, sortedCards) {
+      if (typeof($localStorage.setReviewGrader) === "undefined") {
+        $localStorage.setReviewGrader = {};
+      }
+      $localStorage.setReviewGrader[set.code] = { set: set, sortedCards: sortedCards }; 
+    },
+
+    /**
+     * Sorts cards by color, then alphabetically
+     *
+     * @method sortcards
+     * @param {Array} cards
+     * @return {Array} sorted cards
+     */
+    sortCards: function(cards) {
+      var colorOrder = {
+        'White':        0,
+        'Blue':         1,
+        'Black':        2,
+        'Red':          3,
+        'Green':        4,
+        'Multicolored': 5,
+        'Artifact':     6,
+        'Land':         7
+      };
+
+      var sortByColor = function(a,b) {
+        if (colorOrder[colorOf(a)] > colorOrder[colorOf(b)]) {
+          return 1;
+        } else if (colorOrder[colorOf(a)] < colorOrder[colorOf(b)]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      };
+
+      var sortByName = function(a,b) {
+        if (a > b) { return 1; }
+        if (a < b) { return -1; }
+        return 0;
+      };
+
+      return cards.sort(function(a,b) {
+        var byColor = sortByColor(a,b);
+        if (byColor === 0) {
+          return sortByName(a,b);
+        } else {
+          return byColor;
+        }
+      });
+    },
+
+    /**
+     * Returns the color of the card, handling multicolored or no color
+     *
+     * @method colorOf
+     * @param {Object} : card
+     * @return {String} : e.g., "White", "Artifact", "Land", or "Multicolored"
+     */
+    colorOf: function(card) {
       if (typeof(card.colors) == 'undefined' && card.type.match(/Land/)) {
-        cardsByColor.Land.push(card);
+        return "Land";
       }
       else if (typeof(card.colors) == 'undefined' && card.type.match(/Artifact/)) {
-        cardsByColor.Artifact.push(card);
+        return "Artifact";
       }
       else if (card.colors.length > 1) {
-        cardsByColor.Multicolored.push(card);
+        return "Multicolored";
       }
       else {
-        cardsByColor[card.colors[0]].push(card);
+        return card.colors[0];
       }
-    }
+    },
   };
 });
-
